dc80cc441811a1a7414e3b32a20fd7b480b01844
SergeyMarkelov <avemarksun@gmail.com>
2022-07-24 20:41:07 +0200

    Done

---
+++ src/DoughnutFactory.java
@@ -0,0 +1,21 @@
+public class DoughnutFactory {
+    public Doughnut getDoughnut(DoughnutTypes type) //take ENUM DoughnutTypes
+    // and return object, with interface Doughnut
+    {
+        Doughnut toReturn = null;
+        switch (type) {
+            case CHERRY:
+                toReturn = new CherryDoughnut();
+                break;
+            case CHOCOLATE:
+                toReturn = new ChocolateDoughnut();
+                break;
+            case ALMOND:
+                toReturn = new AlmondDoughnut();
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong doughnut type:" + type);
+        }
+        return toReturn;
+    }
+}
